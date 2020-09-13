b3a0972d0c3774c9d453aab103e78d5673133a5d
Alon Wolenitz <alonsukasa@gmail.com>
2020-09-13 17:00:26 +0300

    Prettified code

--- wikilinks.py
+++ wikilinks.py
@@ -7,10 +7,10 @@ def wikilinks(term, depth, lastpages):
     results = []
     apilink = f'https://he.wikipedia.org/w/api.php?action=query&format=json&titles={term}&prop=links&pllimit=max'
     res = requests.get(apilink).json()
-    pageid = list(res["query"]["pages"].keys())[0]
-    pagetitle = res["query"]["pages"][pageid]["title"]
+    pageid, pagedata = list(res["query"]["pages"].items())[0]
+    pagetitle = pagedata["title"]
     print(pageid + " - " + pagetitle)
-    # Check if link is miising page or model page
+    # Check if link is missing page or model page
     if pageid == "-1" or pagetitle[0:6] == "תבנית:": 
         return "Same"
     # Check if link was already visited and add to visited list
@@ -18,13 +18,12 @@ def wikilinks(term, depth, lastpages):
         visited.append(1)
         return "Same"
     lastpages.append(pageid)
-    links = res["query"]["pages"][pageid]["links"]
+    links = pagedata["links"]
     for i in links:
         results.append(i["title"])
     if depth > 0:
         if len(results) > 0:
             newterm = results[random.randrange(0,len(results))]
-            print(f'New term: {newterm}') 
             nextpage = wikilinks(newterm,depth-1,lastpages)
             if not isinstance(nextpage,list):
                 print("Some error - skipping")
@@ -44,4 +43,4 @@ if __name__ == '__main__':
     link_list = wikilinks(term,3,[])
     print(f'Got {len(link_list)} total links')
     print (f'Got {len(set(link_list))} unique links')
-    print(f"Tried to revisit a link {len(visited)} times")
+    print(f'Tried to revisit a link {len(visited)} times')
